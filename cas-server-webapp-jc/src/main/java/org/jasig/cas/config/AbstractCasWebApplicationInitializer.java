667ebbd5635ae55202eeab4e728c9dc4886ab537
zion64 <zion64@msn.com>
2015-12-11 16:54:25 +0900

    refactoring the resource directory...

--- cas-server-webapp-jc/src/main/java/org/jasig/cas/config/AbstractCasWebApplicationInitializer.java
+++ cas-server-webapp-jc/src/main/java/org/jasig/cas/config/AbstractCasWebApplicationInitializer.java
@@ -3,6 +3,10 @@
  */
 package org.jasig.cas.config;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.List;
 import java.util.Set;
 
@@ -10,8 +14,10 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -39,18 +45,39 @@ public abstract class AbstractCasWebApplicationInitializer
 	
 	private static String CAS_HOME = "/opt/cas";
 	
+	/**
+	 * @param servletContext
+	 * @throws ServletException
+	 */
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		
-		String casHome = this.getCasHome(); 
-		if(!ObjectUtils.isEmpty(casHome)) CAS_HOME = casHome;
-		this.initCasHomeDir();
+		
+		this.initCasEnvironment();
 
 		super.onStartup(servletContext);
 		
 		this.addAdditionalServlet(servletContext);
+
+		
 	}
 
+	/**
+	 * @throws ServletException
+	 */
+	private void initCasEnvironment() throws ServletException {
+		try {
+			this.initCasHomeDir();
+			this.copyResourcesToCasHome();
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			throw new ServletException(new IOException(e.getMessage()));
+		}
+	}
+
+	/**
+	 * @return
+	 */
 	@Override
 	protected WebApplicationContext createRootApplicationContext() {
 		Class<?>[] configClasses = getRootConfigClasses();
@@ -58,10 +85,10 @@ public abstract class AbstractCasWebApplicationInitializer
 			AnnotationConfigWebApplicationContext rootAppContext = new AnnotationConfigWebApplicationContext();
 
 			String[] springProfiles = this.getSpringProfiles();
-			String displayName = this.getDisplayName();
-
 			if (!ObjectUtils.isEmpty(springProfiles))
 				rootAppContext.getEnvironment().setActiveProfiles(springProfiles);
+
+			String displayName = this.getDisplayName();
 			if (!ObjectUtils.isEmpty(displayName))
 				rootAppContext.setDisplayName(displayName);
 
@@ -117,21 +144,42 @@ public abstract class AbstractCasWebApplicationInitializer
 		return mappings;
 	}
 
+	/**
+	 * @return
+	 */
 	protected abstract String[] getAdditionalServletMappingsForDispatcher();
 
+	/**
+	 * @return
+	 */
 	protected abstract List<ServletSpec> getAdditionalServlet();
 
+	/**
+	 * @return
+	 */
 	protected abstract String[] getSpringProfiles();
 
+	/**
+	 * @return
+	 */
 	protected abstract String getDisplayName();
 	
+	/**
+	 * @return
+	 */
 	protected abstract String getCasHome();
 
+	/**
+	 * @return
+	 */
 	@Override
 	protected String getServletName() {
 		return CAS_SERVLET_NAME;
 	}
 
+	/**
+	 * @param servletContext
+	 */
 	protected void addAdditionalServlet(ServletContext servletContext) {
 		this.registAdditionalServlet(new ServletSpec(servletContext, "metrics-health", new HealthCheckServlet(), 2,
 				new String[] { "/statistics/healthcheck" }));
@@ -147,6 +195,10 @@ public abstract class AbstractCasWebApplicationInitializer
 		}
 	}
 
+	/**
+	 * @param spec
+	 * @return
+	 */
 	private ServletRegistration.Dynamic registAdditionalServlet(ServletSpec spec) {
 		ServletRegistration.Dynamic servlet = spec.getServletContext().addServlet(spec.getServletName(),
 				spec.getServlet());
@@ -157,6 +209,10 @@ public abstract class AbstractCasWebApplicationInitializer
 		return servlet;
 	}
 
+	/**
+	 * @param mappingConflicts
+	 * @param servletName
+	 */
 	private void checkServletUrlMappingConflict(Set<String> mappingConflicts, String servletName) {
 		if (!mappingConflicts.isEmpty()) {
 			for (String s : mappingConflicts) {
@@ -176,10 +232,32 @@ public abstract class AbstractCasWebApplicationInitializer
 	 *   CAS_HOME/i18n : message sources directory
 	 *   CAS_HOME/conf : cas configuration directory
 	 * 
-	 * @return
+	 * @throws IOException 
 	 */
-	private Boolean initCasHomeDir() {
+	private void initCasHomeDir() throws IOException {
+
+		String casHome = this.getCasHome(); 
+		if(!ObjectUtils.isEmpty(casHome)) CAS_HOME = casHome;
+
+		FileSystemResource resource = new FileSystemResource(CAS_HOME);
 		
-		return true;
+		if(resource.exists()) {
+			if(resource.getFile().isFile()) {
+				log.error("CAS_HOME is not a directory. The current CAS_HOME in this configuration is {}", CAS_HOME);
+				throw new FileAlreadyExistsException(CAS_HOME);
+			} else {
+				FileUtils.forceMkdir(new File(CAS_HOME + "/logs"));
+				FileUtils.forceMkdir(new File(CAS_HOME + "/conf"));
+				FileUtils.forceMkdir(new File(CAS_HOME + "/i18n"));
+				FileUtils.forceMkdir(new File(CAS_HOME + "/services"));
+			}
+		} else {
+			log.error("CAS_HOME is not exist. Current CAS_HOME is {}", CAS_HOME);
+			throw new FileNotFoundException("CAS_HOME is not exist. Current CAS_HOME is " + CAS_HOME);
+		}
+	}
+	
+	private void copyResourcesToCasHome() {
+		
 	}
 }
